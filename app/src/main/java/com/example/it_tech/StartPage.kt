e1ef303ea8249648ba67d55162fa32bbe037a3a1
Gosamerr <loikliza4@gmail.com>
2024-12-11 01:03:49 +0300

    delete old xml rename new xml for cash_reg, period_reg, start_page change construction of period_reg connect xml and kt

--- app/src/main/java/com/example/it_tech/StartPage.kt
+++ app/src/main/java/com/example/it_tech/StartPage.kt
@@ -9,17 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 class StartPage : AppCompatActivity() {
 
     private lateinit var helloUser: TextView
-
     private lateinit var startButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_page)
+        setContentView(R.layout.activity_start)
 
 
-        helloUser = findViewById(R.id.Hello)
-        startButton = findViewById(R.id.start_button)
+        helloUser = findViewById(R.id.topTextView)
+        startButton = findViewById(R.id.registerTextView)
 
         startButton.setOnClickListener {
             val intent = Intent(this, Cash_reg:: class.java)
